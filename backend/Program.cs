46b06591bfc7811f48ab8755107c899a2c65b693
fujitashuichi <fujitashuu14@gmail.com>
2026-01-17 13:35:25 +0900

    Add: バックエンドのクラスの原型を作成

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,6 +1,48 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/tasks", () =>
+{
+    return TaskStore.Tasks;
+});
+
+app.MapPost("/tasks", (TaskItem task) =>
+{
+    if (task.Hours <= 0 || string.IsNullOrWhiteSpace(task.Title))
+    {
+        return new string("無効なタスクデータです。タイトルは空でなく、時間は正の数である必要があります。");
+    }
+    try {
+        TaskItem newTask = new()
+        {
+            Id = Guid.NewGuid(),
+            Title = task.Title,
+            Hours = task.Hours,
+            IsCompleted = task.IsCompleted
+        };
+        TaskStore.Tasks.Add(newTask);
+        return new string("タスクは正常に追加されました");
+    } catch (Exception ex)
+    {
+        return new string("タスクの追加に失敗しました: " + ex.Message);
+    }
+});
 
 app.Run();
+
+
+public class TaskItem
+{
+    public Guid Id = Guid.NewGuid();
+    public required string Title { get; set; }
+    public double Hours { get; set; }
+    public bool IsCompleted { get; set; }
+};
+
+public class TaskStore
+{
+    public static List<TaskItem> Tasks = new([
+        new TaskItem { Title = "Dummy_1", Hours = 1.5, IsCompleted = false },
+        new TaskItem { Title = "Dummy_2", Hours = 2.0, IsCompleted = true }
+    ]);
+};
